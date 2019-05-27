9c450bc229cd54ae69809dcfaea42fd3a7b1a49d
na.oki <naoki.sushitokyo@gmail.com>
2019-05-27 22:56:39 +0000

    user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_074940) do
+ActiveRecord::Schema.define(version: 2019_05_27_075459) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id"
+    t.bigint "{:foreign_key=>true}_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["{:foreign_key=>true}_id"], name: "index_tasks_on_{:foreign_key=>true}_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
